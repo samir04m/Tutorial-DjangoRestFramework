2199f9a552d54049093aa92d5e5e3a78684f6710
Samir Mejia <samir04m@gmail.com>
2020-05-11 17:54:09 -0500

    Archivos faltantes

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -12,5 +12,9 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('', include('snippets.urls')),
 ]
+
+    
